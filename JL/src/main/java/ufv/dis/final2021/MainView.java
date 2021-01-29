9a9b2b06afbf079abbc53287e91602a2edd09a97
JuanLatasa <juanan.latasa@gmail.com>
2021-01-29 19:37:07 +0100

    Actualizacion

--- JL/src/main/java/ufv/dis/final2021/MainView.java
+++ JL/src/main/java/ufv/dis/final2021/MainView.java
@@ -14,8 +14,25 @@ import com.vaadin.flow.router.Route;
 public class MainView extends VerticalLayout {
 
     public MainView() {
+
         Button button = new Button("Click me",
                 event -> Notification.show("Clicked!"));
         add(button);
     }
+    public String longToIp(long ip) {
+        StringBuilder result = new StringBuilder(15);
+        for (int i = 0; i < 4; i++) { result.insert(0,Long.toString(ip & 0xff)); if (i < 3) {
+            result.insert(0,'.'); }
+            ip = ip >> 8; }
+        return result.toString(); }
+
+
+    public static Long Dot2LongIP(String dottedIP) {
+        String[] addrArray = dottedIP.split("\\."); long num = 0;
+        for (int i=0;i<addrArray.length;i++) {
+            int power = 3-i;
+            num += ((Integer.parseInt(addrArray[i]) % 256) *
+                    Math.pow(256, power));
+        }
+        return num; }
 }
